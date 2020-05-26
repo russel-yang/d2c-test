1e05d004eed8b8df85c9769b1bc511f47a09fe12
Sean LeBlanc (T2 SF) <sean.leblanc@take2games.com>
2020-05-26 17:53:09 -0400

    use danger github util instead of fs

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,5 +1,4 @@
 import { danger, warn } from 'danger';
-import { readFileSync } from 'fs';
 
 const dangerousChanges = [
   '.deleteContentType(',
@@ -17,16 +16,21 @@ const dangerousChanges = [
   'deleted: true'
 ];
 
-danger.git.created_files.concat(danger.git.modified_files).forEach(path => {
-  const file = readFileSync(path).toString();
-  const lines = file.split('\n');
-  lines.forEach((line, lineNumber) => {
-    if (dangerousChanges.some(i => line.includes(i))) {
-      warn(
-        'Risky migration: make sure this is reviewed by QA before merging',
-        path,
-        lineNumber
-      );
-    }
-  });
+Promise.all(
+  danger.git.created_files.concat(danger.git.modified_files).map(async path => {
+    const file = await danger.github.utils.fileContents(path);
+    const lines = file.split('\n');
+    lines.forEach((line, lineNumber) => {
+      if (dangerousChanges.some(i => line.includes(i))) {
+        warn(
+          'Risky migration: make sure this is reviewed by QA before merging',
+          path,
+          lineNumber
+        );
+      }
+    });
+  })
+).catch(err => {
+  console.error(err);
+  process.exit(1);
 });
