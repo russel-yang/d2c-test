ccbd1aa7cee7541be06d4ef65622c87951f4a6f7
Sean LeBlanc (T2 SF) <sean.leblanc@take2games.com>
2020-05-26 18:29:12 -0400

    update to work with both ci and local

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,5 @@
 import { danger, warn } from 'danger';
+import { readFile } from 'fs';
 
 const dangerousChanges = [
   '.deleteContentType(',
@@ -17,22 +18,29 @@ const dangerousChanges = [
 ];
 
 Promise.all(
-    const file = await danger.github.utils.fileContents(path);
-    const lines = file.split('\n');
-    lines.forEach((line, lineNumber) => {
-      if (dangerousChanges.some(i => line.includes(i))) {
-        warn(
-          'Risky migration: make sure this is reviewed by QA before merging',
-          path,
-          lineNumber
-        );
-      }
-    });
-  })
   danger.git.created_files
     .concat(danger.git.modified_files)
     .filter(path => path.startsWith('scripts/active/'))
     .map(async path => {
+      const file = await (danger.github
+        ? danger.github.utils.fileContents(path)
+        : new Promise<string>((resolve, reject) =>
+            readFile(path, (err, data) =>
+              err ? reject(err) : resolve(data.toString())
+            )
+          ));
+      const lines = file.split('\n');
+      lines.forEach((line, lineNumber) => {
+        if (dangerousChanges.some(i => line.includes(i))) {
+          console.log(path, lineNumber);
+          warn(
+            'Risky migration: make sure this is reviewed by QA before merging',
+            path,
+            lineNumber
+          );
+        }
+      });
+    })
 ).catch(err => {
   console.error(err);
   process.exit(1);
